d42190aba0681e25c053ba092b9477d2df7d64da
Kraken32bin <kraken32bin@protonmail.com>
2021-04-24 23:03:26 -0300

    Removed unused function from obj_file object

--- src/reader.cpp
+++ src/reader.cpp
@@ -185,8 +185,8 @@ struct obj_file
         file.close();
     }
 
-    void data_buffer(unsigned int idx)
-    {
+    // void data_buffer(unsigned int idx)
+    // {
         /*m_idx = idx;
         unsigned int buffer;
         glGenBuffers(1, &buffer);
@@ -210,7 +210,7 @@ struct obj_file
             indices, 
             GL_STATIC_DRAW
         );*/
-    }
+    // }
 
     void draw_mesh()
     {
@@ -245,25 +245,3 @@ int main()
 
     return 0;
 }
-
-/**
- * unsigned int buffer;
- * glGenBuffers(1, &buffer);
- * glBindBuffer(GL_ARRAY_BUFFER, buffer);
- * glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
- * 
- * glEnableVertexAttribArray(0);
- * glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
- * 
- * unsigned int ibo;
- * glGenBuffers(1, &ibo);
- * glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
- * glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
- * 
- * 
- * before...
- * glDrawArrays(GL_TRIANGLES, 0, 6);
- * 
- * when drawing
- * glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr); // this draws the elements
- */
