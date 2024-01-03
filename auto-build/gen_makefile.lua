593ab07b7e07249c0b079ff3b7a23ed8e5ca190e
GooseCMD <kraken32bin@protonmail.com>
2024-01-03 20:29:04 -0300

    feat(build_system): added conditional generation

--- auto-build/gen_makefile.lua
+++ auto-build/gen_makefile.lua
@@ -19,22 +19,36 @@ local g_allFuncs = {
 local g_outputString = ''
 
 local function puts(in_string)
-    print(in_string)
+    io.write(in_string)
     -- g_outputString = g_outputString .. in_string
 end
 
 local function generateFormatPrint(in_fmtFuncNAme)
-    puts('define ' .. in_fmtFuncNAme)
-    puts('\t$(SS)echo $(1)')
-    puts('endef\n')
-end 
+    puts('define ' .. in_fmtFuncNAme .. '\n')
+    puts('\t$(SS)echo $(1)\n')
+    puts('endef\n\n')
+end
+
+local function generateConditional(inVar)
+    local fmt_string = string.format('ifeq ($(%s),%s)\n',
+                                    inVar.variable_name, inVar.value)
+    puts(fmt_string)
+
+    if inVar.second ~= nil then
+        puts('else\n')
+    end
+
+    puts('endif\n')
+end
 
 local funcEnum = {
     FormatFunc = 1,
+    FormatConditional = 2,
 }
 
 local translateTable = {
     [funcEnum.FormatFunc] = generateFormatPrint,
+    [funcEnum.FormatConditional] = generateConditional,
 }
 
 local function dispatchGenFunc(genType, genInfo)
@@ -43,6 +57,10 @@ end
 
 local function generateConfigs(inConfigs)
     g_allFuncs:insertFunc(funcEnum.FormatFunc, 'fmt')
+    g_allFuncs:insertFunc(funcEnum.FormatConditional, {
+        variable_name = 'OS',
+        value = 'Windows_NT',
+    })
 end
 
 function GenerateAll(inConfigs)
