f68c26e8679a0200eacf58e758d79b69c3e306d7
GooseCMD <kraken32bin@protonmail.com>
2024-01-03 23:49:16 -0300

    feat(build_system): added gen of insides of conditionals

--- auto-build/gen_makefile.lua
+++ auto-build/gen_makefile.lua
@@ -23,8 +23,10 @@ local function puts(in_string)
     -- g_outputString = g_outputString .. in_string
 end
 
-local function generateFormatPrint(in_fmtFuncNAme)
-    puts('define ' .. in_fmtFuncNAme .. '\n')
+local dispatchAllGenFunc
+
+local function generateFormatPrint(inVar)
+    puts('define ' .. inVar .. '\n')
     puts('\t$(SS)echo $(1)\n')
     puts('endef\n\n')
 end
@@ -34,25 +36,42 @@ local function generateConditional(inVar)
                                     inVar.variable_name, inVar.value)
     puts(fmt_string)
 
-    if inVar.second ~= nil then
-        puts('else\n')
+    if inVar.inside ~= nil then
+        dispatchAllGenFunc(inVar.inside)
     end
 
-    puts('endif\n')
+    if inVar.second ~= nil then
+        puts('else\n')
+        dispatchAllGenFunc(inVar.second)
+    end
+
+    puts('endif\n\n')
+end
+
+local function generateVariableEq(inVar)
+    puts(string.format('%s = %s\n', inVar.variable_name, inVar.value))
 end
 
 local funcEnum = {
-    FormatFunc = 1,
+    FormatFunc        = 1,
     FormatConditional = 2,
+    FormatVariableEq  = 3,
 }
 
 local translateTable = {
-    [funcEnum.FormatFunc] = generateFormatPrint,
+    [funcEnum.FormatFunc]        = generateFormatPrint,
     [funcEnum.FormatConditional] = generateConditional,
+    [funcEnum.FormatVariableEq]  = generateVariableEq,
 }
 
 local function dispatchGenFunc(genType, genInfo)
     translateTable[genType](genInfo)
+end
+
+function dispatchAllGenFunc(inList)
+    for _, value in pairs(inList) do
+        dispatchGenFunc(value.genType, value.genInfo)
+    end
 end
 
 local function generateConfigs(inConfigs)
@@ -60,6 +79,15 @@ local function generateConfigs(inConfigs)
     g_allFuncs:insertFunc(funcEnum.FormatConditional, {
         variable_name = 'OS',
         value = 'Windows_NT',
+        inside = {
+            [1] = {
+                genType = funcEnum.FormatVariableEq,
+                genInfo = {
+                    variable_name = 'dos',
+                    value = 'Windows'
+                }
+            },
+        }
     })
 end
 
@@ -68,7 +96,5 @@ function GenerateAll(inConfigs)
 
     local allFuncs = g_allFuncs:getAll()
 
-    for index, value in pairs(allFuncs) do
-        dispatchGenFunc(value.genType, value.genInfo)
-    end
+    dispatchAllGenFunc(allFuncs)
 end
